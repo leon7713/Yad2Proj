e1bdecd451d00c953f06cec529c4b9225fa65b03
Leonid <leonushar@gmail.com>
2021-03-11 18:11:44 +0200

    UI fixes

--- Yad2Proj/Controllers/HomeController.cs
+++ Yad2Proj/Controllers/HomeController.cs
@@ -8,33 +8,34 @@ using Yad2Proj.Models;
 
 namespace Yad2Proj.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IRepositoryOf<int, Product> _products;
-        private readonly IRepositoryOf<int, User> _users;
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
+      private readonly IRepositoryOf<int, Product> _products;
+      private readonly IRepositoryOf<int, User> _users;
 
-        public HomeController(ILogger<HomeController> logger, IRepositoryOf<int, Product> products, IRepositoryOf<int, User> users)
-        {
-            _logger = logger;
-            _products = products;
-            _users = users;
-        }
+      public HomeController(ILogger<HomeController> logger, IRepositoryOf<int, Product> products, IRepositoryOf<int, User> users)
+      {
+         _logger = logger;
+         _products = products;
+         _users = users;
+      }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult ShowAll()
-        {
-            var products = _products.GetAll();
-            return View(products);
-        }
-        public IActionResult Privacy()
-        {
-            ViewBag.MainName = "Main Page";
-            return View();
-        }
+      public IActionResult Index()
+      {
+         ViewBag.MainName = "Main Page";
+         return View();
+      }
+      public IActionResult ShowAll()
+      {
+         var products = _products.GetAll();
+         return View(products);
+      }
+      public IActionResult Privacy()
+      {
+         ViewBag.MainName = "Main Page";
+         return View();
+      }
 
       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
       public IActionResult Error()
