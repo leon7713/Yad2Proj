79f1f502a443cf3fc12e1d36c981084cf4e5cf0d
Yotam Glick <yotamglick@gmail.com>
2021-03-20 10:43:08 +0200

    Cart

--- Yad2Proj/Controllers/ProductController.cs
+++ Yad2Proj/Controllers/ProductController.cs
@@ -10,21 +10,21 @@ using Yad2Proj.Models.ProductViewModels;
 
 namespace Yad2Proj.Controllers
 {
-   [Authorize]
-   public class ProductController : Controller
-   {
-      private readonly ILogger<HomeController> _logger;
-      private readonly IRepositoryOf<int, Product> _products;
-      private readonly IRepositoryOf<int, User> _users;
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepositoryOf<int, Product> _products;
+        private readonly IRepositoryOf<int, User> _users;
 
-      public ProductController(ILogger<HomeController> logger,
-         IRepositoryOf<int, Product> products,
-         IRepositoryOf<int, User> users)
-      {
-         _logger = logger;
-         _products = products;
-         _users = users;
-         //var p = products.GetById(4);
+        public ProductController(ILogger<HomeController> logger,
+           IRepositoryOf<int, Product> products,
+           IRepositoryOf<int, User> users)
+        {
+            _logger = logger;
+            _products = products;
+            _users = users;
+            //var p = products.GetById(4);
 
             //FileStream fs = new FileStream(@"C:\Users\yotam\Pictures\pic.jpg", FileMode.Open, FileAccess.Read);
             //MemoryStream ms = new MemoryStream();
@@ -36,46 +36,46 @@ namespace Yad2Proj.Controllers
             //_products.Update(4, p);
         }
 
-      [Authorize]
-      public IActionResult AddItem()
-      {
-         ViewBag.MainName = "Add Item's Page";
+        [Authorize]
+        public IActionResult AddItem()
+        {
+            ViewBag.MainName = "Add Item's Page";
 
-         Product product = new Product();
-         return View(product);
-      }
+            Product product = new Product();
+            return View(product);
+        }
 
-      [HttpPost]
-      public async Task<IActionResult> AddItem(ProductViewModel productViewModel)
-      {
-         ViewBag.MainName = "Add Item's Page";
+        [HttpPost]
+        public async Task<IActionResult> AddItem(ProductViewModel productViewModel)
+        {
+            ViewBag.MainName = "Add Item's Page";
 
-         if (ModelState.IsValid)
-         {
-            var userId = int.Parse(Request.Cookies["UserId"]); //34
-            //int userId = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "Authentication").Value);
-            var owner = _users.GetById(userId);
-
-            var product = new Product
+            if (ModelState.IsValid)
             {
-               //Id = productViewModel.Id,
-               Owner = owner,
-               User = owner,
-               Title = productViewModel.Title,
-               ShortDesc = productViewModel.ShortDesc,
-               LongDesc = productViewModel.LongDesc,
-               Price = productViewModel.Price,
-               Timestamp = productViewModel.Timestamp,
-               InCart = productViewModel.InCart,
-               State = productViewModel.State,
-            };
+                var userId = int.Parse(Request.Cookies["UserId"]); //34
+                                                                   //int userId = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "Authentication").Value);
+                var owner = _users.GetById(userId);
 
-            if (productViewModel.Image1 != null)
-               product.Image1 = await productViewModel.Image1.GetBytes();
-            if (productViewModel.Image2 != null)
-               product.Image2 = await productViewModel.Image2.GetBytes();
-            if (productViewModel.Image3 != null)
-               product.Image3 = await productViewModel.Image3.GetBytes();
+                var product = new Product
+                {
+                    //Id = productViewModel.Id,
+                    Owner = owner,
+                    User = owner,
+                    Title = productViewModel.Title,
+                    ShortDesc = productViewModel.ShortDesc,
+                    LongDesc = productViewModel.LongDesc,
+                    Price = productViewModel.Price,
+                    Timestamp = productViewModel.Timestamp,
+                    InCart = productViewModel.InCart,
+                    State = productViewModel.State,
+                };
+
+                if (productViewModel.Image1 != null)
+                    product.Image1 = await productViewModel.Image1.GetBytes();
+                if (productViewModel.Image2 != null)
+                    product.Image2 = await productViewModel.Image2.GetBytes();
+                if (productViewModel.Image3 != null)
+                    product.Image3 = await productViewModel.Image3.GetBytes();
 
                 _products.Create(product);
             }
@@ -83,50 +83,58 @@ namespace Yad2Proj.Controllers
             return View();
         }
 
-      [AllowAnonymous]
-      public IActionResult GetImage1(int id)
-      {
-         var product = _products.GetById(id);
+        [AllowAnonymous]
+        public IActionResult GetImage1(int id)
+        {
+            var product = _products.GetById(id);
 
-         if (product == null || product.Image1 == null)
-            return NotFound();
+            if (product == null || product.Image1 == null)
+                return NotFound();
 
-         return new FileContentResult(product.Image1, "image/jpg");
-      }
+            return new FileContentResult(product.Image1, "image/jpg");
+        }
 
-      [AllowAnonymous]
-      public IActionResult GetImage2(int id)
-      {
-         var product = _products.GetById(id);
+        [AllowAnonymous]
+        public IActionResult GetImage2(int id)
+        {
+            var product = _products.GetById(id);
 
-         if (product == null || product.Image2 == null)
-            return NotFound();
+            if (product == null || product.Image2 == null)
+                return NotFound();
 
-         return new FileContentResult(product.Image2, "image/jpg");
-      }
+            return new FileContentResult(product.Image2, "image/jpg");
+        }
 
-      [AllowAnonymous]
-      public IActionResult GetImage3(int id)
-      {
-         var product = _products.GetById(id);
+        [AllowAnonymous]
+        public IActionResult GetImage3(int id)
+        {
+            var product = _products.GetById(id);
 
-         if (product == null || product.Image3 == null)
-            return NotFound();
+            if (product == null || product.Image3 == null)
+                return NotFound();
 
-         return new FileContentResult(product.Image3, "image/jpg");
-      }
+            return new FileContentResult(product.Image3, "image/jpg");
+        }
 
-      [HttpGet]
-      [AllowAnonymous]
-      public IActionResult Details(int id)
-      {
-         ViewBag.MainName = "More Details";
-         var productWithUser = _products.GetByIdJoin(p => p.Id == id, u => u.Owner).First();
-         if (productWithUser == null)
-         {
-            NotFound();
-         }
-         return View(productWithUser);
-      }
-   }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Details(int id)
+        {
+            ViewBag.MainName = "More Details";
+            var productWithUser = _products.GetByIdJoin(p => p.Id == id, u => u.Owner).First();
+            if (productWithUser == null)
+            {
+                NotFound();
+            }
+            return View(productWithUser);
+        }
+        public IActionResult Cart(int id)
+        {
+            ViewBag.MainName = "More Details";
+            var userId = int.Parse(Request.Cookies["UserId"]);
+            var owner = _users.GetById(userId);
+            var productWithUser = _products.GetAll().Where(p => p.Owner == owner);
+            return View(productWithUser);
+        }
+    }
 }
